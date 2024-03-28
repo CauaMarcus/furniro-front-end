974462ef8457b8b2a05643d5b40d7ea962868c99
CauaMarcus <caua.cardoso600@gmail.com>
2024-03-27 22:53:50 -0300

    Banner & Background Creation
    Added Background and Banner with a Buy Now link and a copy phrase from the Banner.

--- furniro-front-end/src/app/Commons/components/HomeBanner.tsx
+++ furniro-front-end/src/app/Commons/components/HomeBanner.tsx
@@ -1,10 +1,32 @@
 import Image from "next/image";
 import FurnitureRoom from "../images/background.png";
+import Link from "next/link";
 
 const HomeBanner = () => {
   return (
-    <div>
-      <Image className="w-full h-full" src={FurnitureRoom} alt="Background" />
+    <div className="relative w-full">
+      <Image className="w-full" src={FurnitureRoom} alt="Background" />
+      <div
+        className="absolute top-[225px] right-[50px]  box-border
+       bg-lightOrange border-lightOrange border-4"
+      >
+        <div className="mx-10 my-8 w-96 flex flex-col gap-4 text-zinc-800">
+          <p className="text-xs font-semibold w-96">New Arrival</p>
+          <p className="w-1/3 font-bold text-beige text-4xl">
+            Discover Our New Collection
+          </p>
+          <p className="text-xs w-96">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit, vitae
+            deleniti mollitia magni.
+          </p>
+          <div
+            className="mx-8 flex justify-center items-center text-white
+          font-bold text-xs box-border bg-beige border-beige border-4 w-36 h-10"
+          >
+            <Link href={"*"}>BUY NOW</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
